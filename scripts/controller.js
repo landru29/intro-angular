694d8dac84fbc67ac95061cbda453f37971a7db6
Meichel Cyrille <cmeichel@digitaleo.com>
2014-04-02 14:06:42 +0200

    setting up angularjs

---
+++ scripts/controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('introApp')
+        .controller('MainCtrl', function($scope) {
+
+            $scope.menu= [
+            	'Bonjour les gens',
+            	'Hello world',
+            	'Guten tag menchen',
+            	'Ola'
+            ];
+
+        });
